139f1c402fb91d3e67aeaec7b65d1511399da67f
Denis Mwangi <deniskimani2022@gmail.com>
2025-03-14 13:58:29 +0300

    style: update layout order of sections in Home component for improved structure

--- app/page.js
+++ app/page.js
@@ -4,24 +4,23 @@ import Work from "@/Components/Work";
 import Blog from "@/Components/Blog";
 import Footer from "@/Components/Footer";
 import React from "react";
-import "./globals.css";
 
 export default function Home() {
   return (
-    <main>
-      <section id="landing">
+    <main className="flex flex-col min-h-screen">
+      <section id="landing" className="order-1">
         <LandingPart />
       </section>
-      <section id="about">
+      <section id="about" className="order-2">
         <Aboutme />
       </section>
-      <section id="work">
+      <section id="work" className="order-3">
         <Work />
       </section>
-      <section id="blog">
+      <section id="blog" className="order-4">
         <Blog />
       </section>
-      <section id="contact">
+      <section id="contact" className="order-5">
         <Footer />
       </section>
     </main>
