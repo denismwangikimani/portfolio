1e42e850040bbcd6d50c639fb0570150c95d9e44
Denis Mwangi <deniskimani2022@gmail.com>
2025-03-15 17:15:42 +0300

    feat: add AboutMe component and integrate it into the homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,14 @@
 import PersonalTagCard from "@/components/Landing_part";
+import AboutMe from "@/components/AboutMe";
 
 export default function Home() {
   return (
     <main className="max-w-[1440px] mx-auto bg-white">
       <section id="landing">
         <PersonalTagCard />
+      </section>
+      <section id="about">
+        <AboutMe />
       </section>
     </main>
   );
